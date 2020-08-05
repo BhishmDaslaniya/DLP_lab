fd3ed0ab2948139206ceddbae2c72b23ba48801d
Bhishm Daslaniya <bhishm1999@gmail.com>
2020-08-05 10:21:33 +0530

    practical 4 complete

---
+++ Practical_4/lines.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#define PI 3.14
+int main()
+{
+	int r = 5; // radius
+	printf("%d\n",PI*r*r);//area
+	return 0;
+}
---
+++ Practical_4/test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+	// This is test Program
+	int r = 5;
+	/* calculate area 
+	area = 3.14*r*r */
+	printf("%d\n",3.14*r*r);
+}
