d64be02cfce2fac8856dda780dd02a8bd6147934
Bhishm Daslaniya <bhishm1999@gmail.com>
2020-08-12 10:59:38 +0530

    practical 5 complete

---
+++ Practical_5/test_input.c
@@ -0,0 +1,20 @@
+// Input without preprocessor directives...
+/* 
+	Author : Bhishm Daslaniya [17CE023]	
+*/
+
+int main(){
+	int sum = 1;
+	for(int i = 0 ; i < 10; i+=1){
+		sum = sum<<i;
+		printf("Iteration %d\n",i);
+	}
+	scanf("sum : %d",&sum);
+	float f = 1.2222;
+	char c = 'c';
+	char ch ='cdcd';
+	char str[100] = "Copyright by Bhishm Daslaniya";
+	int 1b = 55;
+	++sum;
+	return 0;
+}
